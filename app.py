d1799ce5f303332797fb0b850dddc9526c54095a
dip <dipaleepatil2019@gmail.com>
2025-12-30 11:16:47 +0530

    Fix port binding for Render

--- app.py
+++ app.py
@@ -124,6 +124,6 @@ def view_paste(paste_id):
 
     return render_template("view.html", content=paste["content"])
 
-# ---------- Run ----------
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
